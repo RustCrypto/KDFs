9408c8ddd7195d39ec4dfa4523c1bc4a8c1e42f7
David Drysdale <dmd@lurklurk.org>
2021-02-27 10:52:28 +0000

    Add Wycheproof HKDF test vectors
    Created from:
    https://github.com/google/wycheproof/blob/2196000605e45d91097147c9c71f26b72af58003/testvectors/
    
    files:
     - hkdf_sha1_test.json
     - hkdf_sha256_test.json
     - hkdf_sha384_test.json
     - hkdf_sha512_test.json

--- hkdf/tests/tests.rs
+++ hkdf/tests/tests.rs
@@ -4,7 +4,7 @@ use hex;
 
 use hkdf::{Hkdf, HkdfExtract};
 use sha1::Sha1;
-use sha2::Sha256;
+use sha2::{Sha256, Sha384, Sha512};
 
 struct Test<'a> {
     ikm: &'a str,
@@ -329,3 +329,48 @@ fn test_extract_streaming() {
         num_concatted += 1;
     }
 }
+
+/// Define test
+macro_rules! new_test {
+    ($name:ident, $test_name:expr, $hkdf:ty) => {
+        #[test]
+        fn $name() {
+            use blobby::Blob4Iterator;
+
+            fn run_test(ikm: &[u8], salt: &[u8], info: &[u8], okm: &[u8]) -> Option<&'static str> {
+                let prk = <$hkdf>::new(Some(salt), ikm);
+                let mut got_okm = vec![0; okm.len()];
+
+                if prk.expand(info, &mut got_okm).is_err() {
+                    return Some("prk expand");
+                }
+                if got_okm != okm {
+                    return Some("mismatch in okm");
+                }
+                None
+            }
+
+            let data = include_bytes!(concat!("data/", $test_name, ".blb"));
+
+            for (i, row) in Blob4Iterator::new(data).unwrap().enumerate() {
+                let [ikm, salt, info, okm] = row.unwrap();
+                if let Some(desc) = run_test(ikm, salt, info, okm) {
+                    panic!(
+                        "\n\
+                         Failed test №{}: {}\n\
+                         ikm:\t{:?}\n\
+                         salt:\t{:?}\n\
+                         info:\t{:?}\n\
+                         okm:\t{:?}\n",
+                        i, desc, ikm, salt, info, okm
+                    );
+                }
+            }
+        }
+    };
+}
+
+new_test!(wycheproof_sha1, "wycheproof-sha1", Hkdf::<Sha1>);
+new_test!(wycheproof_sha256, "wycheproof-sha256", Hkdf::<Sha256>);
+new_test!(wycheproof_sha384, "wycheproof-sha384", Hkdf::<Sha384>);
+new_test!(wycheproof_sha512, "wycheproof-sha512", Hkdf::<Sha512>);
