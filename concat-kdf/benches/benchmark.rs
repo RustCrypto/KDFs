98bc33fd750ec47ddab6eacd631e3d2baac7eab9
Artyom Pavlov <newpavlov@gmail.com>
2022-09-13 00:57:58 +0000

    concat-kdf: remove criterion dependency (#72)

--- concat-kdf/benches/benchmark.rs
+++ concat-kdf/benches/benchmark.rs
@@ -1,18 +1,20 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+#![feature(test)]
+extern crate test;
+
 use sha2::{Sha224, Sha256, Sha512};
+use test::Bencher;
 
 macro_rules! define_benchmark {
-    ($function_name:ident, $digest_name:ident) => {
-        fn $function_name(c: &mut Criterion) {
-            c.bench_function(concat!("Concat KDF ", stringify!($digest_name)), |b| {
-                b.iter(|| {
-                    concat_kdf::derive_key_into::<$digest_name>(
-                        &[0u8; 32],
-                        &[0u8; 32],
-                        &mut [0u8; 32],
-                    )
-                    .unwrap();
-                })
+    ($name:ident, $hash:ty) => {
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            let secret = [0u8; 32];
+            let info = [0u8; 32];
+            let mut key = [0u8; 32];
+            b.iter(|| {
+                let (secret, info, key) = test::black_box((&secret, &info, &mut key));
+                concat_kdf::derive_key_into::<$hash>(secret, info, key).unwrap();
+                test::black_box(key);
             });
         }
     };
@@ -21,11 +23,3 @@ macro_rules! define_benchmark {
 define_benchmark!(concat_kdf_sha224, Sha224);
 define_benchmark!(concat_kdf_sha256, Sha256);
 define_benchmark!(concat_kdf_sha512, Sha512);
-
-criterion_group!(
-    benches,
-    concat_kdf_sha224,
-    concat_kdf_sha256,
-    concat_kdf_sha512
-);
-criterion_main!(benches);
