0a994890554c1e5604066e21af540fbe3a516919
Arthur Gautier <arthur.gautier@arista.com>
2023-10-29 14:31:56 -0700

    cleanup

--- kbkdf/src/sealed.rs
+++ kbkdf/src/sealed.rs
@@ -1,13 +1,12 @@
-    use digest::{
-        consts::{U16, U24, U32, U8},
-        generic_array::typenum::Unsigned,
-    };
+use digest::{
+    consts::{U16, U24, U32, U8},
+    generic_array::typenum::Unsigned,
+};
 
-    /// Marker used to register valid values for R in the KBKDF
-    pub trait R: Unsigned {}
+/// Marker used to register valid values for R in the KBKDF
+pub trait R: Unsigned {}
 
-    impl R for U8 {}
-    impl R for U16 {}
-    impl R for U24 {}
-    impl R for U32 {}
-
+impl R for U8 {}
+impl R for U16 {}
+impl R for U24 {}
+impl R for U32 {}
