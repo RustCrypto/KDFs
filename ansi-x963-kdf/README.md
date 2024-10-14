80622b1b99457ac2927100c5fafef2245eb069bf
nemynm <180121731+nemynm@users.noreply.github.com>
2024-10-13 23:41:23 -0400

    Add ANSI-X9.63-KDF (#102)
    Adding support for ANSI X9.63 Key Derivation Function (ANSI-X9.63-KDF).
    
    ANSI-X9.63-KDF is defined in [SEC.1](https://www.secg.org/sec1-v2.pdf)
    and is the recommended KDF to be used for CMS with ECC - as per [RFC
    5753](https://datatracker.ietf.org/doc/html/rfc5753).
    
    This would close issue https://github.com/RustCrypto/KDFs/issues/101.
    
    **Overview:**
    This pull request:
     - adds `ansi-x963-kdf` crate with:
        - `ansi_x963_kdf::derive_key_into` function
        - tests for SHA254, SHA256 and SHA512 hash functions
     - updates repository `README.md` with `ansi-x963-kdf`
    
    **Notes:**
    
    - As concat-kdf and ANSI-X9.63-KDF are close and essentially differ in
    the order in which the data is concatenated, most of the code for this
    ansi-x963-kdf implementation has been adapted from concat-kdf.
    - I did not find public test vectors, so the test cases have been
    generated using a Java-based implementation of ANSI-X9.63-KDF
    - I added one "unsafe" test case to test the input overflow case. As I
    am not an "unsafe" specialist, carefull review of this one is
    particularly appreciated.

---
+++ ansi-x963-kdf/README.md
@@ -0,0 +1,59 @@
+# RustCrypto: ANSI X9.63 Key Derivation Function (ANSI-X9.63-KDF)
+
+[![crate][crate-image]][crate-link]
+[![Docs][docs-image]][docs-link]
+![Apache2/MIT licensed][license-image]
+![Rust Version][rustc-image]
+[![Project Chat][chat-image]][chat-link]
+[![Build Status][build-image]][build-link]
+
+Pure Rust implementation of the ANSI X9.63 Key Derivation Function (ANSI-X9.63-KDF) generic over hash function. 
+This function is described in the section 3.6.1 of [SEC 1: Elliptic Curve Cryptography](http://www.secg.org/sec1-v2.pdf).
+
+# Usage
+
+The most common way to use ANSI-X9.63-KDF is as follows: you generate a shared secret with other party (e.g. via Diffie-Hellman algorithm) 
+and use key derivation function to derive a shared key.
+
+```rust
+let mut key = [0u8; 32];
+ansi_x963_kdf::derive_key_into::<sha2::Sha256>(b"shared-secret", b"other-info", &mut key).unwrap();
+```
+
+## Minimum Supported Rust Version
+
+Rust **1.72** or higher.
+
+Minimum supported Rust version can be changed in the future, but it will be
+done with a minor version bump.
+
+## SemVer Policy
+
+- All on-by-default features of this library are covered by SemVer
+- MSRV is considered exempt from SemVer as noted above
+
+## License
+
+Licensed under either of:
+
+* [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+* [MIT license](http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
+
+[crate-image]: https://img.shields.io/crates/v/ansi-x963-kdf.svg
+[crate-link]: https://crates.io/crates/ansi-x963-kdf
+[docs-image]: https://docs.rs/ansi-x963-kdf/badge.svg
+[docs-link]: https://docs.rs/ansi-x963-kdf/
+[license-image]: https://img.shields.io/badge/license-Apache2.0/MIT-blue.svg
+[rustc-image]: https://img.shields.io/badge/rustc-1.72+-blue.svg
+[chat-image]: https://img.shields.io/badge/zulip-join_chat-blue.svg
+[chat-link]: https://rustcrypto.zulipchat.com/#narrow/stream/260043-KDFs
+[build-image]: https://github.com/RustCrypto/KDFs/workflows/ansi-x963-kdf/badge.svg?branch=master&event=push
+[build-link]: https://github.com/RustCrypto/KDFs/actions?query=workflow:ansi-x963-kdf
---
+++ ansi-x963-kdf/src/lib.rs
@@ -0,0 +1,123 @@
+//! An implementation of ANSI-X9.63 KDF Key Derivation Function.
+//!
+//! This function is described in the section 3.6.1 of [SEC 1: Elliptic Curve Cryptography][1].
+//!
+//! # Usage
+//!
+//! The most common way to use ANSI-X9.63 KDF is as follows: you generate a shared secret
+//! with other party (e.g. via Diffie-Hellman algorithm) and use key derivation function
+//! to derive a shared key.
+//!
+//! ```rust
+//! let mut key = [0u8; 32];
+//! ansi_x963_kdf::derive_key_into::<sha2::Sha256>(b"shared-secret", b"other-info", &mut key).unwrap();
+//! ```
+//!
+//! [1]: https://www.secg.org/sec1-v2.pdf
+
+#![no_std]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+
+use core::fmt;
+use digest::{array::typenum::Unsigned, Digest, FixedOutputReset};
+
+#[cfg(feature = "std")]
+extern crate std;
+
+/// ANSI-X9.63 KDF errors.
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Error {
+    /// The length of the secret is zero.
+    NoSecret,
+    /// The length of the output is zero.
+    NoOutput,
+    /// The length of the input is too big
+    InputOverflow,
+    /// The length of the output is too big.
+    CounterOverflow,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        f.write_str(match self {
+            Error::NoSecret => "Buffer for secret has zero length.",
+            Error::NoOutput => "Buffer for key has zero length.",
+            Error::InputOverflow => "Input length is to big.",
+            Error::CounterOverflow => "Requested key length is to big.",
+        })
+    }
+}
+
+impl ::core::error::Error for Error {}
+
+/// Derives `key` in-place from `secret` and `shared_info`.
+/// ```rust
+/// use hex_literal::hex;
+/// let mut key = [0u8; 42];
+/// ansi_x963_kdf::derive_key_into::<sha2::Sha256>(b"top-secret", b"info", &mut key).unwrap();
+/// assert_eq!(key, hex!("85397c03b3894cdc12e7e042698d040f449dbff97a86d0a4dd2d0a4409b8d969e01e57091cf170dfd977"));
+/// ```
+pub fn derive_key_into<D>(secret: &[u8], shared_info: &[u8], key: &mut [u8]) -> Result<(), Error>
+where
+    D: Digest + FixedOutputReset,
+{
+    if secret.is_empty() {
+        return Err(Error::NoSecret);
+    }
+
+    if key.is_empty() {
+        return Err(Error::NoOutput);
+    }
+
+    // 1. Check that |Z| + |SharedInfo| + 4 < hashmaxlen
+    // where "hashmaxlen denote the maximum length in octets of messages that can be hashed using Hash".
+    // N.B.: `D::OutputSize::U64 * (u32::MAX as u64)`` is currently used as an approximation of hashmaxlen.
+    if secret.len() as u64 + shared_info.len() as u64 + 4 >= D::OutputSize::U64 * (u32::MAX as u64)
+    {
+        return Err(Error::InputOverflow);
+    }
+
+    // 2. Check that keydatalen < hashlen × (2^32 − 1)
+    if key.len() as u64 >= D::OutputSize::U64 * (u32::MAX as u64) {
+        return Err(Error::CounterOverflow);
+    }
+
+    let mut digest = D::new();
+
+    // 3. Initiate a 4 octet, big-endian octet string Counter as 00000001
+    let mut counter: u32 = 1;
+
+    // 4. For i = 1 to keydatalen/hashlen,
+    for chunk in key.chunks_mut(D::OutputSize::USIZE) {
+        // 4.1 Compute Ki = Hash(Z ‖ Counter ‖ [SharedInfo]) using the selected hash function
+        Digest::update(&mut digest, secret);
+        Digest::update(&mut digest, counter.to_be_bytes());
+        Digest::update(&mut digest, shared_info);
+        chunk.copy_from_slice(&digest.finalize_reset()[..chunk.len()]);
+        // 4.2. Increment Counter
+        counter += 1;
+    }
+
+    Ok(())
+}
+
+/// Derives and returns `length` bytes key from `secret` and `shared_info`.
+/// ```rust
+/// use hex_literal::hex;
+/// let key = ansi_x963_kdf::derive_key::<sha2::Sha256>(b"top-secret", b"info", 42).unwrap();
+/// assert_eq!(key, hex!("85397c03b3894cdc12e7e042698d040f449dbff97a86d0a4dd2d0a4409b8d969e01e57091cf170dfd977"));
+/// ```
+#[cfg(feature = "std")]
+#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
+pub fn derive_key<D>(
+    secret: &[u8],
+    shared_info: &[u8],
+    length: usize,
+) -> Result<std::vec::Vec<u8>, Error>
+where
+    D: Digest + FixedOutputReset,
+{
+    let mut key = std::vec![0u8; length];
+    derive_key_into::<D>(secret, shared_info, &mut key)?;
+    Ok(key)
+}
