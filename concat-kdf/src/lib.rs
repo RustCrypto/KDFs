619dc620fac0c91ca8bddbf5a6e77a8cf1385586
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2024-10-14 15:21:12 +0300

    Remove `alloc` crate feature from `concat-kdf` and `ansi-x963-kdf`

--- concat-kdf/src/lib.rs
+++ concat-kdf/src/lib.rs
@@ -5,32 +5,6 @@
 use core::fmt;
 use digest::{array::typenum::Unsigned, Digest, FixedOutputReset, Update};
 
-#[cfg(feature = "alloc")]
-extern crate alloc;
-
-/// Concat KDF errors.
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub enum Error {
-    /// The length of the secret is zero.
-    NoSecret,
-    /// The length of the output is zero.
-    NoOutput,
-    /// The length of the output is too big.
-    CounterOverflow,
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        f.write_str(match self {
-            Error::NoSecret => "Buffer for secret has zero length.",
-            Error::NoOutput => "Buffer for key has zero length.",
-            Error::CounterOverflow => "Requested key length is to big.",
-        })
-    }
-}
-
-impl ::core::error::Error for Error {}
-
 /// Derives `key` in-place from `secret` and `other_info`.
 ///
 /// # Example
@@ -73,26 +47,25 @@ where
     Ok(())
 }
 
-/// Derives and returns `length` bytes key from `secret` and `other_info`.
-///
-/// # Example
-/// ```rust
-/// use hex_literal::hex;
-/// use sha2::Sha256;
-///
-/// let key = concat_kdf::derive_key::<Sha256>(b"secret", b"shared-info", 16).unwrap();
-/// assert_eq!(key[..], hex!("960db2c549ab16d71a7b008e005c2bdc")[..]);
-/// ```
-#[cfg(feature = "alloc")]
-pub fn derive_key<D>(
-    secret: &[u8],
-    other_info: &[u8],
-    length: usize,
-) -> Result<alloc::boxed::Box<[u8]>, Error>
-where
-    D: Digest + FixedOutputReset,
-{
-    let mut key = alloc::vec![0u8; length].into_boxed_slice();
-    derive_key_into::<D>(secret, other_info, &mut key)?;
-    Ok(key)
+/// Concat KDF errors.
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Error {
+    /// The length of the secret is zero.
+    NoSecret,
+    /// The length of the output is zero.
+    NoOutput,
+    /// The length of the output is too big.
+    CounterOverflow,
 }
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        f.write_str(match self {
+            Error::NoSecret => "Buffer for secret has zero length.",
+            Error::NoOutput => "Buffer for key has zero length.",
+            Error::CounterOverflow => "Requested key length is to big.",
+        })
+    }
+}
+
+impl core::error::Error for Error {}
