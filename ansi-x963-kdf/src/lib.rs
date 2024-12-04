02c319c2f5eed1f4749ff4d7f73fff273569b40f
Artyom Pavlov <newpavlov@gmail.com>
2024-12-04 08:58:08 +0300

    Remove `alloc` crate feature from `concat-kdf` and `ansi-x963-kdf` (#106)
    The crate feature is used only for the 3-line convenience functions,
    which can be easily written by users and probably extremely rarely used
    in practice.

--- ansi-x963-kdf/src/lib.rs
+++ ansi-x963-kdf/src/lib.rs
@@ -1,38 +1,8 @@
 #![no_std]
 #![doc = include_str!("../README.md")]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 use core::fmt;
 use digest::{array::typenum::Unsigned, Digest, FixedOutputReset};
-
-#[cfg(feature = "alloc")]
-extern crate alloc;
-
-/// ANSI-X9.63 KDF errors.
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub enum Error {
-    /// The length of the secret is zero.
-    NoSecret,
-    /// The length of the output is zero.
-    NoOutput,
-    /// The length of the input is too big
-    InputOverflow,
-    /// The length of the output is too big.
-    CounterOverflow,
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        f.write_str(match self {
-            Error::NoSecret => "Buffer for secret has zero length.",
-            Error::NoOutput => "Buffer for key has zero length.",
-            Error::InputOverflow => "Input length is to big.",
-            Error::CounterOverflow => "Requested key length is to big.",
-        })
-    }
-}
-
-impl ::core::error::Error for Error {}
 
 /// Derives `key` in-place from `secret` and `shared_info`.
 ///
@@ -90,27 +60,28 @@ where
     Ok(())
 }
 
-/// Derives and returns `length` bytes key from `secret` and `shared_info`.
-///
-/// # Example
-/// ```
-/// use hex_literal::hex;
-/// use sha2::Sha256;
-///
-/// let key = ansi_x963_kdf::derive_key::<Sha256>(b"secret", b"shared-info", 16).unwrap();
-/// assert_eq!(key[..], hex!("8dbb1d50bcc7fc782abc9db5c64a2826")[..]);
-/// ```
-#[cfg(feature = "alloc")]
-#[inline]
-pub fn derive_key<D>(
-    secret: &[u8],
-    shared_info: &[u8],
-    length: usize,
-) -> Result<alloc::boxed::Box<[u8]>, Error>
-where
-    D: Digest + FixedOutputReset,
-{
-    let mut key = alloc::vec![0u8; length].into_boxed_slice();
-    derive_key_into::<D>(secret, shared_info, &mut key)?;
-    Ok(key)
+/// ANSI-X9.63 KDF errors.
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Error {
+    /// The length of the secret is zero.
+    NoSecret,
+    /// The length of the output is zero.
+    NoOutput,
+    /// The length of the input is too big
+    InputOverflow,
+    /// The length of the output is too big.
+    CounterOverflow,
 }
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        f.write_str(match self {
+            Error::NoSecret => "Buffer for secret has zero length.",
+            Error::NoOutput => "Buffer for key has zero length.",
+            Error::InputOverflow => "Input length is to big.",
+            Error::CounterOverflow => "Requested key length is to big.",
+        })
+    }
+}
+
+impl core::error::Error for Error {}
