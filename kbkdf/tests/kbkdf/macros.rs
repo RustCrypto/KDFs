d7664e9ca71ae8bbaf9bd0c2a814c2878dbdd77b
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2025-01-24 13:11:10 +0200

    feat(kbkdf): add counter mode tests;

---
+++ kbkdf/tests/kbkdf/macros.rs
@@ -0,0 +1,17 @@
+
+macro_rules! test_counter_mode {
+    ($name:ident, $prf:ty, $out_len:ty, $r:ty, $test_cases:expr) => {
+        #[test]
+        fn $name() {
+            for test_case in $test_cases {
+                let counter = kbkdf::Counter::<$prf, $out_len, $r>::default();
+
+                let key = counter
+                    .derive(test_case.kin, false, false, test_case.label, test_case.context)
+                    .unwrap();
+
+                assert_eq!(test_case.kout[..], key[..]);
+            }
+        }
+    };
+}
---
+++ kbkdf/tests/kbkdf/main.rs
@@ -0,0 +1,41 @@
+#[macro_use]
+mod macros;
+
+mod counter_mode;
+
+struct CounterModeTestData {
+    kin: &'static [u8],
+    label: &'static [u8],
+    context: &'static [u8],
+    kout: &'static [u8],
+}
+
+type HmacSha1 = hmac::Hmac<sha1::Sha1>;
+type HmacSha224 = hmac::Hmac<sha2::Sha224>;
+type HmacSha256 = hmac::Hmac<sha2::Sha256>;
+type HmacSha384 = hmac::Hmac<sha2::Sha384>;
+type HmacSha512 = hmac::Hmac<sha2::Sha512>;
+
+struct MockOutputU128;
+
+impl digest::crypto_common::KeySizeUser for MockOutputU128 {
+    type KeySize = digest::consts::U16;
+}
+
+struct MockOutputU160;
+
+impl digest::crypto_common::KeySizeUser for MockOutputU160 {
+    type KeySize = digest::consts::U20;
+}
+
+struct MockOutputU320;
+
+impl digest::crypto_common::KeySizeUser for MockOutputU320 {
+    type KeySize = digest::consts::U40;
+}
+
+struct MockOutputU256;
+
+impl digest::crypto_common::KeySizeUser for MockOutputU256 {
+    type KeySize = digest::consts::U32;
+}
